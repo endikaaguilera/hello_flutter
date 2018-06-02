993a50e20a862e103bd81316f718e375b25ea547
endika <matiax28@gmail.com>
2018-06-02 17:48:12 +0200

    User Interaction implemented

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -1,16 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
-import 'package:hello_flutter/unit.dart';
+import 'unit.dart';
 
-/// Converter screen where users can input amounts to convert.
-///
-/// Currently, it just displays a list of mock units.
+const _padding = EdgeInsets.all(16.0);
+
+/// [ConverterRoute] where users can input amounts to convert in one [Unit]
+/// and retrieve the conversion in another [Unit] for a specific [Category].
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
-// TODO: Make ConverterRoute a StatefulWidget
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   /// This [Category]'s name.
   final String name;
 
@@ -29,16 +33,49 @@ class ConverterRoute extends StatelessWidget {
         assert(color != null),
         assert(units != null);
 
-  // TODO: Create State object for the ConverterRoute
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  // TODO: Set some variables, such as for keeping track of the user's input
+  // value and units
+
+  // TODO: Determine whether you need to override anything, such as initState()
+
+  // TODO: Add other helper functions. We've given you one, _format()
+
+  /// Clean up conversion; trim trailing zeros, e.g. 5.500 -> 5.5, 10.0 -> 10
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Here is just a placeholder for a list of mock units
-    // TODO: Once the build() function is inside the State object,
-    // you'll have to reference this using `widget.units`
-    final unitWidgets = units.map((Unit unit) {
+    // TODO: Create the 'input' group of widgets. This is a Column that includes
+    // includes the output value, and 'from' unit [Dropdown].
+
+    // TODO: Create a compare arrows icon.
+
+    // TODO: Create the 'output' group of widgets. This is a Column that
+
+    // TODO: Return the input, arrows, and output widgets, wrapped in
+
+    // TODO: Delete the below placeholder code
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@
 import 'package:flutter/material.dart';
 
 // You can use a relative import, i.e. `import 'category_route.dart;'` or
-// a package import, as shown below.
+// a package import.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'package:hello_flutter/category_route.dart';
+import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
@@ -21,6 +21,16 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
+      theme: ThemeData(
+        fontFamily: 'Raleway',
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.grey[600],
+        ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
       home: CategoryRoute(),
     );
   }
